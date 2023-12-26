17ab14892aa5afbb2be020a66ca3c34dc05fa7ff
DEV_AMARNADH <116832906+dev6112@users.noreply.github.com>
2023-12-26 11:45:41 +0530

    app.py

--- app.py
+++ app.py
@@ -74,6 +74,6 @@ def download(filename):
     return send_file(filename, as_attachment=True)
 
 #if __name__ == "__main__":
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
+    #if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        #os.makedirs(app.config['UPLOAD_FOLDER'])
     #app.run(debug=True)
