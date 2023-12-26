be10cce56547c927fc69f4393c15965ba066823b
DEV_AMARNADH <116832906+dev6112@users.noreply.github.com>
2023-12-26 13:19:51 +0530

    app.py

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def download(filename):
 if __name__ == "__main__":
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-app.run(debug=True)
+app.run(debug=Flase)
